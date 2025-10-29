c67c035cf5dbe6109b1baa8c6e24ab9dc6472d94
James Brown <b.james322@gmail.com>
2025-10-29 17:03:38 -0400

    add ulid to goals

--- app/Models/Goal.php
+++ app/Models/Goal.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Goal extends Model
 {
     /** @use HasFactory<\Database\Factories\GoalFactory> */
-    use HasFactory;
+    use HasFactory, HasUlids;
 }
