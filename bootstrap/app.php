e4fa08bc53f063ba412b85ae422cd4bffdfcba3e
James Brown <b.james322@gmail.com>
2025-11-26 17:10:07 -0500

    remove route logger

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,6 @@
 
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\RouteLogger;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -21,7 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
-            RouteLogger::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
