482191bf535c54b8c2ce6887d5b34de79baafeae
James Brown <b.james322@gmail.com>
2025-11-26 21:37:38 -0500

    add mailgun to services

--- config/services.php
+++ config/services.php
@@ -41,4 +41,11 @@ return [
         'redirect_url' => env('WORKOS_REDIRECT_URL'),
     ],
 
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
+
 ];
